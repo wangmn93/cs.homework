fd59dc983c991115774b084d27318a4d7e4b33b3
mengnan wang <wangmn93@gmail.com>
2017-05-15 21:39:39 +0200

    修改rl ex04

--- rlss17/e04/ex04-2.py
+++ rlss17/e04/ex04-2.py
@@ -21,6 +21,7 @@ goal = (4,12)
 environment = Maze(structure, (5, 13),initPos=[(4,1)]) #initial state and unreachable final state
 task = MDPMazeTask(environment)
 q_value = {}
+state_count = {}
 actions = [0,1,2,3]
 alpha  = .1
 gamma = 1
@@ -115,15 +116,18 @@ def lambda_TD(trace,lambda_=.1,agent = 'SARSA'):
         # print q_value[(current,action)]
         i+=1
 
+def reduce_eps(eps,state):
+    eps *= 1/(state_count[state]+.1)
+    return eps
 
 
-
-def learning(agent='SARSA',lambda_TD_on=True,maxEpisode = 400,lambda_=.1):
+def learning(agent='SARSA',lambda_TD_on=True,maxEpisode = 400,lambda_=.1,explore_function=False):
     #initialize q_value
     for i in range(1,height+1,1):
         for j in range(1,width+1,1):
             for k in actions:
                 q_value[((i,j),k)] = 0.
+                state_count[(i,j)] = 0.
 
     steps = 0
     steps2 = 0
@@ -146,12 +150,21 @@ def learning(agent='SARSA',lambda_TD_on=True,maxEpisode = 400,lambda_=.1):
         current = environment.perseus  # S
         if agent != 'SARSA':
             # off-policy
-            action = eps_greedy(current)  #A
+            if explore_function:
+                eps = reduce_eps(.1,current)
+            else:
+                eps = .1
+            action = eps_greedy(current,eps)  #A
         environment.performAction(action)# perform action
         next = environment.perseus #S'
+        state_count[next]+=1 #update state count
         reward = getReward(next)#R
         reward2 += reward
-        action2 = eps_greedy(next) #A'
+        if explore_function:
+            eps = reduce_eps(.1, next)
+        else:
+            eps = .1
+        action2 = eps_greedy(next,eps) #A'
         # print current, action,getReward(next), next
         trace.append((current,action,reward))
         #SARSA
@@ -206,6 +219,14 @@ def visualizePolicy():
                 print d[a],
         print "|"
 
+
+def visualizeCount():
+    for i in range(1, height + 1, 1):
+        for j in range(1, width + 1, 1):
+            print '|'+str(state_count[(i,j)]),
+        print "|"
+
+
 if __name__ == "__main__":
     # q_value[((3,3),1)]=1
     # q_value[((3, 3), 0)] = 5
@@ -213,22 +234,39 @@ if __name__ == "__main__":
     # print MC_Reward(trace)
     # lambda_TD(trace)
     # print MC_Reward(trace[:1])
-    # steps_sarsa, reward_sarsa = learning('SARSA',lambda_TD_on=True,maxEpisode = 400,lambda_=.2)
-    # visualizePolicy()
-    # plt.plot(moving_average(reward_sarsa), 'r', label='SARSA')
-    for lambda_ in [.1,.2,.3,.4,.5,.6,.7,.8,.9]:
-        steps_q, reward_q = learning('Q',lambda_TD_on=True,maxEpisode = 400,lambda_ = lambda_)
-        #visualizePolicy()
-        #plt.plot(moving_average(reward_q), 'b', label='Q')
+    steps_sarsa, reward_sarsa = learning('SARSA',lambda_TD_on=False,maxEpisode = 400,explore_function=False)
+    visualizePolicy()
+    visualizeCount()
+    plt.plot(moving_average(reward_sarsa), 'r', label='SARSA')
 
+    steps_q, reward_q = learning('Q', lambda_TD_on=False, maxEpisode=400,explore_function=False)
+    visualizePolicy()
+    visualizeCount()
+    plt.plot(moving_average(reward_q), 'b', label='Q')
 
+    pylab.legend(loc='upper left')
+    print 'SARSA mean reward after 300 iterations ',mean(reward_sarsa[300:])
+    print 'SARSA mean steps after 300 iterations ',mean(steps_sarsa[300:])
 
-        #pylab.legend(loc='upper left')
-        # print 'SARSA mean reward after 300 iterations ',mean(reward_sarsa[300:])
-        # print 'SARSA mean steps after 300 iterations ',mean(steps_sarsa[300:])
-        print 'lambda ',lambda_
-        print 'Q mean reward after 300 iterations ',mean(reward_q[300:])
-        print 'Q mean steps after 300 iterations ',mean(steps_q[300:])
-        # plt.bar(np.arange(len(reward_list)),reward_list)
-        #plt.show()
+    print 'Q mean reward after 300 iterations ', mean(reward_q[300:])
+    print 'Q mean steps after 300 iterations ', mean(steps_q[300:])
+    # plt.bar(np.arange(len(reward_list)),reward_list)
+    plt.show()
+
+    #choose lambda for q learning
+    # for lambda_ in [.1,.2,.3,.4,.5,.6,.7,.8,.9]:
+    #     steps_q, reward_q = learning('Q',lambda_TD_on=True,maxEpisode = 400,lambda_ = lambda_)
+    #     #visualizePolicy()
+    #     #plt.plot(moving_average(reward_q), 'b', label='Q')
+    #
+    #
+    #
+    #     #pylab.legend(loc='upper left')
+    #     # print 'SARSA mean reward after 300 iterations ',mean(reward_sarsa[300:])
+    #     # print 'SARSA mean steps after 300 iterations ',mean(steps_sarsa[300:])
+    #     print 'lambda ',lambda_
+    #     print 'Q mean reward after 300 iterations ',mean(reward_q[300:])
+    #     print 'Q mean steps after 300 iterations ',mean(steps_q[300:])
+    #     # plt.bar(np.arange(len(reward_list)),reward_list)
+    #     #plt.show()
 
